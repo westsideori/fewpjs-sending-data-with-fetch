edb740b0b53531876854cacffae99adf38f4dbd3
Ori Markowitz <ori.markowitz@gmail.com>
2021-01-19 18:04:02 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData(userName, userEmail) {
+    const obj = {name: userName, email: userEmail}
+    return fetch("http://localhost:3000/users", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json'
+        },
+        body: JSON.stringify(obj)
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log("success", data)
+            const newP = document.createElement("p")
+            const id = data.id
+            newP.textContent = id
+            document.body.append(newP)
+
+        })
+        .catch( function ( error ) {
+            document.body.innerHTML = error.message
+          } )
+}
+
+submitData("ori", "om")
